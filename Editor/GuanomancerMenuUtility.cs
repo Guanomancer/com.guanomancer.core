e4c70e8896bd42ecc150faa1dad8c111f021b10c
Troels <trowi@guanomancer.com>
2026-03-28 14:29:28 +0100

    Example added

--- Editor/GuanomancerMenuUtility.cs
+++ Editor/GuanomancerMenuUtility.cs
@@ -8,19 +8,10 @@ namespace Guanomancer.Editor
         [MenuItem("GameObject/Guanomancer/Create Category Object", false, 10)]
         private static void CreateCategoryObject(MenuCommand menuCommand)
         {
-            // 1. Create a new GameObject
             GameObject go = new GameObject("New Category");
-            
-            // 2. Add the specific behaviour
             go.AddComponent<CategoryBehaviour>();
-
-            // 3. Ensure it gets reparented correctly if right-clicking an existing object
             GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
-
-            // 4. Register the creation in the Undo system
             Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
-
-            // 5. Select the new object
             Selection.activeObject = go;
         }
     }
