b533d93205bb719e92acf8301ef0ff16f633eb37
Troels <78472401+Guanomancer@users.noreply.github.com>
2026-08-21 16:29:30 +0200

    Added missing default Interaction info value.

--- Runtime/Game/IInteraction.cs
+++ Runtime/Game/IInteraction.cs
@@ -7,7 +7,8 @@ namespace Guanomancer
         bool CanInteract(T info);
         bool TryInteract(T info);
 
-        public static bool Can(GameObject go, T info)
+        public static bool Can(GameObject go, out IInteraction<T> interaction) => Can(go, default, out interaction);
+        public static bool Can(GameObject go, T info = default)
         {
             var interaction = go.GetComponentInParent<IInteraction<T>>();
             return interaction != null && interaction.CanInteract(info);
@@ -19,7 +20,8 @@ namespace Guanomancer
             return interaction != null && interaction.CanInteract(info);
         }
 
-        public static bool Try(GameObject go, T info)
+        public static bool Try(GameObject go, out IInteraction<T> interaction) => Try(go, default, out interaction);
+        public static bool Try(GameObject go, T info = default)
         {
             var interaction = go.GetComponentInParent<IInteraction<T>>();
             return interaction != null && interaction.TryInteract(info);
@@ -31,7 +33,8 @@ namespace Guanomancer
             return interaction != null && interaction.TryInteract(info);
         }
 
-        public static bool Can(Component component, T info)
+        public static bool Can(Component component, out IInteraction<T> interaction) => Can(component, out interaction);
+        public static bool Can(Component component, T info = default)
         {
             var interaction = component.gameObject.GetComponentInParent<IInteraction<T>>();
             return interaction != null && interaction.CanInteract(info);
@@ -43,7 +46,8 @@ namespace Guanomancer
             return interaction != null && interaction.CanInteract(info);
         }
 
-        public static bool Try(Component component, T info)
+        public static bool Try(Component component, out IInteraction<T> interaction) => Try(component, out interaction);
+        public static bool Try(Component component, T info = default)
         {
             var interaction = component.gameObject.GetComponentInParent<IInteraction<T>>();
             return interaction != null && interaction.TryInteract(info);
